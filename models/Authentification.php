19cc3c1d766fac8a78050f393f4def977f029d4d
LydieDev <bulonzabadesire@gmail.com>
2023-12-16 08:06:56 +0100

    updates

--- models/Authentification.php
+++ models/Authentification.php
@@ -1,7 +1,33 @@
 <?php
 
     class Authentification{
-
+        public static function generate_verification_code (){
+            $code = mt_rand(100000,999999);
+            return $code;
+        }
+        public static function send_verification_code ($email, $verification_code) {
+            $reciever = $email;
+            $subject = 'Verification code';
+            $message = "Your verification code is ".$verification_code;
+            $headers = "From: <ndule204@gmail.com>";
+            if (mail($reciever, $subject, $message,$headers)) {
+               return true;
+            } else{
+                return false;
+            }
+            
+        }
+    
+        public static function verifying_code($user_code, $generated_code) {
+        
+            if ($user_code==$generated_code){
+                header('location:new_pass.php');
+            }
+            else{
+                echo 'error code incorrect';
+            }
+        
+        }
     }
 
 ?>
